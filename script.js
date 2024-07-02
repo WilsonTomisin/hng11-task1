c727984451ccdbf614e8ef3275665492ec96a6d2
Wilson Tomisin <tommywilson972@gmail.com>
2024-07-02 15:24:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let utcTime = document.getElementById('time');
+let dayOfTheWeek = document.getElementById("day");
+let now = new Date()
+let year = now.getUTCFullYear();
+let month = now.getUTCMonth() + 1; // Months are zero-based, so add 1
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getUTCSeconds();
+let dayIndex = now.getDate()
+let day ;
+
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let dayOfWeek = weekdays[now.getUTCDay()];
+
+utcTime.innerHTML = `${year}-${month}-${dayIndex}  ;  ${hours}:${minutes}am`
+dayOfTheWeek.innerText = dayOfWeek
+
